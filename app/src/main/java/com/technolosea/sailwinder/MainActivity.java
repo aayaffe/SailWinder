c8eaf2a747be1f268b84e661414aa6d93629bae5
Tal-D <tdan.m92@gmail.com>
2019-08-08 01:01:37 +0300

    Actual addition of google location service validation

--- app/src/main/java/com/technolosea/sailwinder/MainActivity.java
+++ app/src/main/java/com/technolosea/sailwinder/MainActivity.java
@@ -1,32 +1,19 @@
 package com.technolosea.sailwinder;
 
-import android.app.Activity;
-import android.widget.Toast;
+import android.os.Bundle;
 
-import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.GoogleApiAvailability;
+import androidx.appcompat.app.AppCompatActivity;
 
-public class NavyLocationManager
-{
-    private int REQUEST_CODE_RECOVER_PLAY_SERVICES = 200;
 
-    public boolean checkGooglePlayServices(Activity activity)
-    {
-        int conneection_status = GoogleApiAvailability.getInstance().isGooglePlayServicesAvailable(activity);
-        if (conneection_status != ConnectionResult.SUCCESS)
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        NavyLocationManager navy_location_manager = new NavyLocationManager();
+        if (navy_location_manager.checkGooglePlayServices(this))
         {
-            GoogleApiAvailability.getInstance().getErrorDialog( activity, conneection_status,
-                    REQUEST_CODE_RECOVER_PLAY_SERVICES).show();
-
-            return false;
         }
-        Toast toast = Toast.makeText(activity,
-                "Successfully connected to Google location API",
-                Toast.LENGTH_SHORT);
-
-        toast.show();
-
-        return true;
-
     }
 }
